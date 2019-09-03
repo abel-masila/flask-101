c3c8f67d9ca595765936797bd33b84ef18f4d832
Abel <abelmasila@gmail.com>
2019-09-03 15:12:43 +0300

    added books route

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
+books = [
+    {
+        'name': 'Java for dummies',
+        'price': 799,
+        'isbn': 343434
+    },
+    {
+        'name': 'C# for dummies',
+        'price': 699,
+        'isbn': 343433
+    }
+]
 
 # Default route
 @app.route("/")
 def helloworld():
     return "Hello World"
+
+
+@app.route("/books")
+def get_books():
+    return jsonify({'books': books})
 
 
 app.run(port=5000)
