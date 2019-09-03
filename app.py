5bd4bd13a41826f9644288f0abdd1cc2cf07e96a
Abel <abelmasila@gmail.com>
2019-09-03 15:21:05 +0300

    added /book/isbn route and logic

--- app.py
+++ app.py
@@ -20,9 +20,24 @@ def helloworld():
     return "Hello World"
 
 
+#GET /books
 @app.route("/books")
 def get_books():
     return jsonify({'books': books})
+#GET /books/ISBN
+
+
+@app.route("/books/<int:isbn>")
+def get_book_by_isbn(isbn):
+    return_value = {}
+    for book in books:
+        if (book['isbn'] == isbn):
+            return_value = {
+                'name': book['name'],
+                'isbn': book['isbn'],
+                'price': book['price']
+            }
+    return jsonify(return_value)
 
 
 app.run(port=5000)
