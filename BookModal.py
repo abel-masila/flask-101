b5240001d2bb051cde09f3f3433ed1630275bab8
Abel <abelmasila@gmail.com>
2019-09-06 13:06:08 +0300

    added all crud methods for the app

--- BookModal.py
+++ BookModal.py
@@ -12,3 +12,43 @@ class Book(db.Model):
     name = db.Column(db.String(80), nullable=False)
     price = db.Column(db.Float, nullable=False)
     isbn = db.Column(db.Integer)
+
+    def add_book(_name, _price, _isbn):
+        new_book = Book(name=_name, price=_price, isbn=_isbn)
+        db.session.add(new_book)
+
+    def get_all_books():
+        return Book.query.all()
+
+    def get_book(_isbn):
+        return Book.query.filter_by(isbn=_isbn).first()
+
+    def delete_book(_isbn):
+        success = Book.query.filter_by(isbn=_isbn).delete()
+        db.session.commit()
+        return bool(success)
+
+    def update_book_price(_isbn, _price):
+        book_to_update = Book.query.filter_by(isbn=_isbn).first()
+        book_to_update.price = _price
+        db.session.commit()
+
+    def update_book_name(_isbn, _name):
+        book_to_update = Book.query.filter_by(isbn=_isbn).first()
+        book_to_update.name = _name
+        db.session.commit()
+
+    def replace_book(_isbn, _name, _price):
+        book_to_update = Book.query.filter_by(isbn=_isbn).first()
+        book_to_update.price = _price
+        book_to_update.name = _name
+        db.session.commit()
+
+    def __repr__(self):
+        book_object = {
+            'name': self.name,
+            'price': self.price,
+            'isbn': self.isbn
+        }
+
+        return json.dumps(book_object)
