7adcffe37f9eee0e80ef9c29c7951a660c28d8b3
Abel <abelmasila@gmail.com>
2019-09-06 15:10:30 +0300

    Added user Model

---
+++ UserModel.py
@@ -0,0 +1,17 @@
+from flask_sqlalchemy import SQLAlchemy
+from settings import app
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+
+    def __repr__(self):
+        return str({
+            'username': self.username,
+            'password': self.password
+        })
